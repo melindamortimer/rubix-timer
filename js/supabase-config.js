e55e22e6521bf0849c272ee870b3d7856ecf7db2
Silver Crocus <2muchhivinlogic@gmail.com>
2026-03-05 00:31:32 +1100

    Add Supabase project config and client CDN

---
+++ js/supabase-config.js
@@ -0,0 +1,4 @@
+// js/supabase-config.js
+
+export const SUPABASE_URL = 'https://tavdupqgisfhtsxgvpwj.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhdmR1cHFnaXNmaHRzeGd2cHdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2MjA4NTYsImV4cCI6MjA4ODE5Njg1Nn0.zhK-p6oN-aJp5JS9X1wOBChLOINSiMAZVOKP7xnDlLg';
